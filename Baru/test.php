f77d19666f41d4af1cea99be33aed2dbc7d341d4
apakaden <normanbintang@hotmail.co.id>
2016-10-26 23:47:35 +0700

    Rapiin fungsi tombol

--- Baru/test.php
+++ Baru/test.php
@@ -8,7 +8,8 @@ $(document).ready(function(){
 	$('#main').hide();
 
 	$("button#0").click(function(){
-        $.getJSON('akinator.php?command=9', function(data) {
+        var url = 'akinator.php?command=9';
+        $.getJSON(url, function(data) {
             x = data;
             document.getElementById("tanya").innerHTML = x[1];
         });
@@ -17,66 +18,35 @@ $(document).ready(function(){
         $('button#9').hide();
     });
 
-    $("button#1").click(function(){
-        $.getJSON('akinator.php?command=1', function(data) {
-            x = data;
-            document.getElementById("tanya").innerHTML = x[1];
+})
 
-            if (x[0] == 1){
-                $("#leader").load("leaderboard.php");
-                $('#tombol').hide();
-                $('button#9').show();
-            }
-        });
+function jawab(cmd){
+    var url = 'akinator.php?command=' + cmd;
+    $.getJSON(url, function(data) {
+        var x = data;
+        document.getElementById("tanya").innerHTML = x[1];
+        if (x[0] == 1){
+            $("#leader").load("leaderboard.php");
+            $('#tombol').hide();
+            $('button#9').show();
+        }
     });
-
-    $("button#2").click(function(){
-        $.getJSON('akinator.php?command=0', function(data) {
-            x = data;
-            document.getElementById("tanya").innerHTML = x[1];
-
-            if (x[0] == 1){
-                $("#leader").load("leaderboard.php");
-                $('#tombol').hide();
-                $('button#9').show();
-            }
-        });
-    });
-
-    $("button#3").click(function(){
-        $.getJSON('akinator.php?command=2', function(data) {
-            x = data;
-            document.getElementById("tanya").innerHTML = x[1];
-
-            if (x[0] == 1){
-                $("#leader").load("leaderboard.php");
-                $('#tombol').hide();
-                $('button#9').show();
-            }
-        });
-    });
-
-    $("button#9").click(function(){
-        $.getJSON('akinator.php?command=9', function(data) {
-            x = data;
-            document.getElementById("tanya").innerHTML = x[1];
-        });
+    if (cmd == 9){
         $('#tombol').show();
         $('button#9').hide();
-    });
+    }
+}
 
-
-})
 </script>
 <button id="0">Mulai</button>
 <div id="main">
 	<div id="tanya"></div>
 	<div id="tombol">
-		<button id="1">YA</button>
-		<button id="2">TIDAK</button>
-        <button id="3">GA TAU</button>
+		<button id="1" onclick="jawab(1)">YA</button>
+		<button id="2" onclick="jawab(0)">TIDAK</button>
+        <button id="3" onclick="jawab(2)">GA TAU</button>
 	</div>
-	<button id="9">Restart</button>
+	<button id="9" onclick="jawab(9)">Restart</button>
 </div>
 <div id="leader"></div>
 </body>
