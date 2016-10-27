dea2d72d589a61dc773a7b85640bc9d54263ac4f
tirtaabimanyu <tirtaabimanyu@gmail.com>
2016-10-27 18:06:36 +0700

    Benerin yang conflict

--- Baru/README.md
+++ Baru/README.md
@@ -4,16 +4,16 @@
 # Fitur yang udah ada
 1. Tombol ya/tidak udah hilang pas beres
 2. Soal udah keshuffle
+3. Leaderboard paling banyak ditebak
 
 # To do list
 * Tambahin:
 	1. Layout
-	2. Leaderboard paling banyak ditebak
-	3. Leaderboard paling banyak diprotes
-	4. Dashboard
-	5. Nampilin riwayat jawaban pas udah selesai
-	6. Tombol protes kalo gak sesuai pikiran player
-	7. Jawaban yang bener kalo diprotes
+	2. Leaderboard paling banyak diprotes
+	3. Dashboard
+	4. Nampilin riwayat jawaban pas udah selesai
+	5. Tombol protes kalo gak sesuai pikiran player
+	6. Jawaban yang bener kalo diprotes
 * Benerin:
 	1. Nama variabel
 	2. Struktur kode
--- Baru/database/database.sql
+++ Baru/database/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Oct 27, 2016 at 12:15 PM
+-- Generation Time: Oct 27, 2016 at 05:58 PM
 -- Server version: 10.1.18-MariaDB
 -- PHP Version: 5.6.20
 
@@ -38,14 +38,46 @@ CREATE TABLE IF NOT EXISTS `data` (
 --
 
 INSERT INTO `data` (`id`, `nama`, `counter`) VALUES
-(1, 'Satu', 18),
+(1, 'Satu', 21),
 (2, 'Dua', 4),
-(3, 'Tiga', 21),
-(4, 'Empat', 5),
+(3, 'Tiga', 22),
+(4, 'Empat', 6),
 (5, 'Lima', 6),
-(6, 'Enam', 2),
-(7, 'Tujuh', 2),
+(6, 'Enam', 4),
+(7, 'Tujuh', 3),
 (8, 'Delapan', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pertanyaan`
+--
+
+CREATE TABLE IF NOT EXISTS `pertanyaan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `1` int(11) NOT NULL,
+  `2` int(11) NOT NULL,
+  `3` int(11) NOT NULL,
+  `4` int(11) NOT NULL,
+  `5` int(11) NOT NULL,
+  `6` int(11) NOT NULL,
+  `7` int(11) NOT NULL,
+  `8` int(11) NOT NULL,
+  `tanya` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `pertanyaan`
+--
+
+INSERT INTO `pertanyaan` (`id`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `tanya`) VALUES
+(1, 0, 1, 1, 0, 1, 0, 1, 0, 'Prima?'),
+(2, 1, 0, 1, 0, 1, 0, 1, 0, 'Ganjil?'),
+(3, 1, 0, 0, 1, 0, 0, 0, 0, 'Kuadrat?'),
+(4, 1, 0, 0, 0, 0, 0, 0, 1, 'Kubik?'),
+(5, 1, 0, 1, 0, 1, 1, 0, 0, 'Hurufnya ada 4?'),
+(6, 0, 0, 1, 0, 0, 0, 0, 0, 'Apakah dia 3?');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
