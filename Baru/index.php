eb4c7e78b64abfddc9b1d082773f1e84dd6a4a4b
apakaden <normanbintang@hotmail.co.id>
2016-10-27 18:29:31 +0700

    Download Jquery

--- Baru/index.php
+++ Baru/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="jquery.min.js"></script>
 <body>
 <script>
 $(document).ready(function(){
